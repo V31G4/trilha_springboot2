78563a799d99639a8a2248ce38e41c56bdf41014
Camille da Veiga <camilledaveiga@outlook.com>
2024-08-28 15:50:03 -0300

    segunda versão

--- trilha_spring2/exercicio/src/main/java/com/trilha2/exercicio/usuario/Usuario.java
+++ trilha_spring2/exercicio/src/main/java/com/trilha2/exercicio/usuario/Usuario.java
@@ -1,5 +1,6 @@
 package com.trilha2.exercicio.usuario;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
@@ -10,8 +11,11 @@ public class Usuario {
 	
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column
 	private long id;
+	@Column
 	private String nome;
+	@Column
 	private String email;
 	
 	public Usuario() {
